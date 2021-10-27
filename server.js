0c0139507752c600bdd227758fe1a27a1ffd0131
Daniel Hill <daniel.s.hill01@gmail.com>
2021-10-27 11:27:15 -0600

    added mongoose connection

--- server.js
+++ server.js
@@ -3,19 +3,32 @@
 ///////////////////////////////
 
 
-
-// get .env variables
+// Get .env variables
 require('dotenv').config();
 
-// pull PORT from .env, give default value of 3001
+// Pull PORT from .env, give default value of 3001
 const { PORT = 3001 } = process.env;
 
-// import
+// Import Mongoose and Express
 const express = require('express');
+const mongoose = require('mongoose')
 
-// create application object
+// Create application object
 const app = express();
 
+
+///////////////////////////////
+// DATABASE CONNECTION
+///////////////////////////////
+
+// Establish Connection
+mongoose.connect(DATABASE_URL)
+
+//Connection Events
+mongoose.connection
+    .on('open', () => console.log("Connected to MongoDB"))
+    .on('close', () => console.log("Couldn't connect to MongoDB"))
+    .on('error', () => console.log(error));
 
 
 ///////////////////////////////
@@ -23,17 +36,14 @@ const app = express();
 ///////////////////////////////
 
 
-
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
 
-
 ///////////////////////////////
 // LISTENER
 ///////////////////////////////
-
 
 
 app.listen(PORT, () => console.log(`Port ${PORT} is up and running...`))
