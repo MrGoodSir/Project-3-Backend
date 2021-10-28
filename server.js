de12d70bc486985f96fffe054ab9825c99b6d81e
Daniel Hill <daniel.s.hill01@gmail.com>
2021-10-28 11:43:10 -0600

    finished back end

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 ///////////////////////////////
 // DEPENDENCIES
-///////////////////////////////
-
-
-// Get .env variables
+//////////////////////////////
 require('dotenv').config();
-
-// Pull PORT from .env, give default value of 3001
 const { PORT = 3001 } = process.env;
-
-// Import Mongoose and Express
 const express = require('express');
 const mongoose = require('mongoose')
-
-// Create application object
 const app = express();
-
-// Import Middleware
 const cors = require('cors');
 const morgan = require('morgan')
+const DATABASE_URL = process.env.DATABASE_URL
 
 
 ///////////////////////////////
@@ -47,7 +37,6 @@ const EnemySchema = new mongoose.Schema({
     armor: Number,
     image: String,
 })
-
 
 const HumanSchema = new mongoose.Schema({
     name: String,
@@ -78,15 +67,29 @@ app.use(express.json()); // parse json bodies
 // ROUTES
 ///////////////////////////////
 
-
+// Test Start Route
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('This is the start page')
+})
+
+// Test Home Route
+app.get('/home', (req, res) => {
+    res.send('This is the Home page')
+})
+
+// Enemy Seed Route
+app.get('/home/seed', (req, res) => {
+    Enemy.deleteMany({}, (error, allEnemies) => {})
+
+    Enemy.create(enemySeed, (error, data) => {
+        res.redirect('/home/enemy')
+    })
 })
 
 // Enemy Index Route
-app.get('/enemy', async (req, res) => {
+app.get('/home/enemy', async (req, res) => {
     try {
-        res.json(await People.find({}))
+        res.json(await Enemy.find({}))
     } catch (error) {
         alert("Man, you really jacked something up...")
     }
@@ -94,9 +97,65 @@ app.get('/enemy', async (req, res) => {
 
 
 // Enemy Create Route
-app.post('/enemy', async (req, res) => {
+app.post('/home/enemy', async (req, res) => {
     try{
         res.json(await Enemy.create(req.body));
+    } catch (error) {
+        alert("It broke. What on Earth did you do...?")
+    }
+})
+
+// Human Seed Route
+app.get('/home/seed', (req, res) => {
+    Human.deleteMany({}, (error, allHumans) => {})
+
+    Human.create(humanSeed, (error, data) => {
+        res.redirect('/home/human')
+    })
+})
+
+// Human Index Route
+app.get('/home/human', async (req, res) => {
+    try {
+        res.json(await Human.find({}))
+    } catch (error) {
+        alert("Man, you really jacked something up...")
+    }
+})
+
+
+// Human Create Route
+app.post('/home/human', async (req, res) => {
+    try{
+        res.json(await Human.create(req.body));
+    } catch (error) {
+        alert("It broke. What on Earth did you do...?")
+    }
+})
+
+// Animal Seed Route
+app.get('/home/seed', (req, res) => {
+    Animal.deleteMany({}, (error, allAnimals) => {})
+
+    Animal.create(animalSeed, (error, data) => {
+        res.redirect('/home/animal')
+    })
+})
+
+// Animal Index Route
+app.get('/home/animal', async (req, res) => {
+    try {
+        res.json(await Animal.find({}))
+    } catch (error) {
+        alert("Man, you really jacked something up...")
+    }
+})
+
+
+// Animal Create Route
+app.post('/home/animal', async (req, res) => {
+    try{
+        res.json(await Animal.create(req.body));
     } catch (error) {
         alert("It broke. What on Earth did you do...?")
     }
