01dc95a11efe0564e1e4910a821790beeac6e21f
Daniel Hill <daniel.s.hill01@gmail.com>
2021-11-01 15:24:22 -0600

    added minor changes

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 // DEPENDENCIES
 //////////////////////////////
 require('dotenv').config();
-const { PORT = 3001 } = process.env;
+const { DATABASE_URL, PORT = 3001 } = process.env;
 const express = require('express');
-const mongoose = require('mongoose')
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const morgan = require('morgan')
-const DATABASE_URL = process.env.DATABASE_URL
+const morgan = require('morgan');
 
 
 ///////////////////////////////
@@ -30,13 +29,17 @@ mongoose.connection
 ///////////////////////////////
 
 
-const MobSchema = new mongoose.Schema({
-    name: String,
-    health: Number,
-    damage: Number,
-    armor: Number,
-    image: String,
-})
+const Schema = mongoose.Schema;
+
+const MobsSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    health: { type: Number, required: true },
+    damage: { type: Number, required: true },
+    armor: { type: Number, required: true },
+    image: { type: String, required: false },
+}, { timestamps: true });
+
+const Mobs = mongoose.model('Mobs', MobsSchema)
 
 
 ///////////////////////////////
@@ -44,9 +47,9 @@ const MobSchema = new mongoose.Schema({
 ///////////////////////////////
 
 
-app.use(cors()); // to prevent cors errors, open acces to all origins
-app.use(morgan('dev')); // logging
-app.use(express.json()); // parse json bodies
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
 
 
 ///////////////////////////////
@@ -58,31 +61,55 @@ app.get('/', (req, res) => {
     res.send('This is the start page')
 })
 
-// Mob Seed Route
-app.get('/seed', (req, res) => {
-    Mob.deleteMany({}, (error, allMobs) => {})
+// // Mob Seed Route
+// app.get('/seed', (req, res) => {
+//     Mobs.deleteMany({}, (error, allMobs) => {})
 
-    Mob.create(mobSeed, (error, data) => {
-        res.redirect('/home')
-    })
-})
+//     Mobs.create(mobSeed, (error, data) => {
+//         res.redirect('/mobs')
+//     })
+// })
 
 // Mob Index Route
-app.get('/home', async (req, res) => {
+app.get('/mobs', async (req, res) => {
     try {
-        res.json(await Mob.find({}))
+        res.json(await Mobs.find({}))
     } catch (error) {
-        alert("Man, you really jacked something up...")
+
+        res.status(400).json(error);
     }
 })
 
 
 // Mob Create Route
-app.post('/home', async (req, res) => {
+app.post('/mobs', async (req, res) => {
     try{
-        res.json(await Mob.create(req.body));
+        res.json(await Mobs.create(req.body));
     } catch (error) {
-        alert("It broke. What on Earth did you do...?")
+
+        res.status(400).json(error);
+    }
+})
+
+// Mob Delete Route
+app.delete('/mobs/:id', async (req, res) => {
+    try{ 
+        res.json(await Mobs.findByIdAndDelete(req.params.id));
+    } catch (error) {
+
+        res.status(400).json(error);
+    }
+})
+
+// Mob Update Route
+app.put('/mobs/:id', async (req, res) => {
+    try {
+        res.json(
+            await Mobs.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        );
+    } catch (error) {
+
+        res.status(400).json(error);
     }
 })
 
