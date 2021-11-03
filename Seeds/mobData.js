b5448f874e50b4ec97635a32388916237fd348b9
Daniel Hill <daniel.s.hill01@gmail.com>
2021-11-03 13:58:33 -0600

    fixed mob data

--- Seeds/mobData.js
+++ Seeds/mobData.js
@@ -4,28 +4,28 @@ module.exports =[
         health: 20,
         damage: 2.5,
         armor: 2,
-        image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/87/Zombie_JE3_BE2.png/revision/latest/scale-to-width-down/163?cb=20191227070025"
+        image: "https://www.minecraft.net/content/dam/games/minecraft/game-characters/WILBMM_Zombie.png"
     },
     {
         name: "Spider",
         health: 16,
         damage: 2,
         armor: 0,
-        image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/1f/Spider_JE4_BE3.png/revision/latest/scale-to-width-down/150?cb=20200108012105",
+        image: "https://preview.redd.it/omxy6o5efse31.png?auto=webp&s=a15a6285b927973b79bf076418b3f6ccf82d2a74",
     },
     {
         name: "Zombified Piglin",
         health: 20,
         damage: 5,
         armor: 2,
-        image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/1c/Zombified_Piglin_BE6.png/revision/latest/scale-to-width-down/150?cb=20201223164833",
+        image: "https://qph.fs.quoracdn.net/main-qimg-7f194339e137e5f727dccae351cfa342",
     },
     {
         name: "Creeper",
         health: 20,
         damage: 22.5,
         armor: 0,
-        image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/5b/Creeper_JE2_BE1.png/revision/latest/scale-to-width-down/128?cb=20191229172043",
+        image: "https://upload.wikimedia.org/wikipedia/en/4/49/Creeper_%28Minecraft%29.png",
     },
     {
         name: "Skeleton",
@@ -39,6 +39,6 @@ module.exports =[
         health: 40,
         damage: 4.5,
         armor: 0,
-        image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Enderman_Screaming_BE.png/revision/latest/scale-to-width-down/300?cb=20200219232457",
+        image: "https://lh3.googleusercontent.com/proxy/mg1FMDUAX9XvrxELKBeZr6jX8DElogjR19pcz9pfioccdDhP0f007sTR5SB1O_Jm71K7HsvZ3rpkAo-2efpAdx5zu4e93suodGMv",
     },
 ]
